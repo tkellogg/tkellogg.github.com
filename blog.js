fa42758c7dce23ab36134af4d42b3729fc25f256
tkellogg <timothy.kellogg@gmail.com>
2011-07-30 10:45:24 -0600

    Added blog.js with backbone

---
+++ blog.js
@@ -0,0 +1,39 @@
+
+function mapBloggerData(data) {
+	if (typeof(data) === 'Array')
+	{
+		for(var i in data) {
+			data[i] = mapBloggerData(data[i]);
+		}
+		return data
+	}
+	else {
+		return _.map(data, function(attr) { return attr.$t ? attr.$t : attr; });
+	}
+}
+
+var BlogPost = Backbone.Model.extend({
+	initialize: function(object) {
+		this.set(mapBloggerData(object));
+	}
+});
+
+var BlogPostCollection = Backbone.Collection.extend({
+	model: BlogPost,
+	url:'http://www.blogger.com/feeds/6849760623609771363/posts/default?alt=json',
+	tipLength: 5,
+
+	getTip: function(callback) {
+		$.getJSON(url + '&max-results=' + this.tipLength, null, function(data) {
+			this.add(mapBloggerData(data));
+			if (callback) {
+				callback(data);
+			}
+		});
+	}
+
+	// TODO: implement local storage via amplify and sync with blogger
+	sync: function() {
+		
+	}
+});
