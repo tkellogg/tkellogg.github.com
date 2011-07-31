ae81aa5b5b968ed5a40f69a0a4a21fb57a1bc48d
tkellogg <tkellogg@kpaonline.com>
2011-07-30 18:13:08 -0600

    rearranged js code, maybe it'll actually run now

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 $(function() {
 	posts = new BlogPostCollection();
-	posts.loadTip();
-
-	var $blog = $('#blog');
-	posts.forEach(function(post) {
-		var div = $blog.append("<div/>");
-		var view = new BlogPostView({el: div, model: post});
-		view.render();
+	posts.loadTip(function() {
+		var $blog = $('#blog');
+		posts.forEach(function(post) {
+			var div = $blog.append("<div/>");
+			var view = new BlogPostView({el: div, model: post});
+			view.render();
+		});
 	});
 });
 
