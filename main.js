b43a42e8f9fa5b9323750e8fecff9cc13c052bf9
tkellogg <tkellogg@kpaonline.com>
2011-08-27 18:25:35 -0600

    Rearranged main into routes

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-
 $(function() {
-	posts = new BlogPostCollection();
-	posts.loadTip(function(m) {
-		alert(m.length + ' but collection was '+ posts.length);
-		blog = new BlogView(posts);
-		blog.bindModels();
-		blog.render();
+	var Workspace = Backbone.Router.extend({
+		routes: {
+			'': 'about',
+			'about': 'about',
+			'blog': 'blog'
+		},
+
+		about: function() {
+
+		},
+
+		blog: function() {
+			var posts = new BlogPostCollection();
+			posts.loadTip(function(m) {
+				alert(m.length + ' but collection was '+ posts.length);
+				blog = new BlogView(posts);
+				blog.bindModels();
+				blog.render();
+			});
+		}
+
 	});
+
 });
 
