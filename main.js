6540e16c1e0fb1cf2f0728c54eddee8cd92cb1c1
tkellogg <tkellogg@kpaonline.com>
2011-08-27 18:40:12 -0600

    updated some text, broke the blog

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ $(function() {
 			$('#blog').show();
 			var posts = new BlogPostCollection();
 			posts.loadTip(function(m) {
-				alert(m.length + ' but collection was '+ posts.length);
 				blog = new BlogView(posts);
 				blog.bindModels();
 				blog.render();
