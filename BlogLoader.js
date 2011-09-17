e8fab8c67dd69be194a243dc1ab99b7bdafbd199
tkellogg <tkellogg@kpaonline.com>
2011-09-17 16:19:17 -0600

    blog posts now cached in localStorage via amplify

--- BlogLoader.js
+++ BlogLoader.js
@@ -17,6 +17,14 @@
 	
 		load: function(callback) {
 			var params = _.extend(this.xhrParams, { 'max-results': this.tipLength });
+			this.doAjax(callback, params);
+		},
+
+		loadRemainder: function(callback) {
+			this.doAjax(callback, this.xhrParams);
+		},
+
+		doAjax: function(callback, params) {
 			$.ajax(this.url, { crossDomain: true, dataType: 'jsonp', data: params, 
 					success: this.onSuccess.butUsing(this, callback) });
 		},
@@ -27,12 +35,30 @@
 
 			if (callback)
 				callback(entry);
+
+			this.loadRemainder(callback);
 		},
 
 		mergeNewPosts: function(data) {
 			this.posts = amplify.store('posts');
 			
-			window.posts = data;
+			if (!this.posts) {
+				this.posts = data;
+			}
+			else {
+				var existing = _.map(this.posts, function(x) { return x.id; });
+				var _new = _(data).chain()
+					.map(function(x) { return x.id; })
+					.difference(existing)
+					.value();
+				this.posts = _(this.posts).chain()
+					.union(_.select(data, function(x) { 
+							return _.any(existing, function(y) { return x.id == y; }); 
+						})
+					)
+					.sortBy(function(x) { return x.published; })
+					.value();
+			}
 			
 			amplify.store('posts', this.posts);
 		}
