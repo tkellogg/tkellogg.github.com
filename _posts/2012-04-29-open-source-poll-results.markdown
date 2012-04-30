d79c3767d82a7e13d0bce22c1ae848ca09ea28e7
tkellogg <timothy.kellogg@gmail.com>
2012-04-29 22:25:20 -0600

    Added data and JS charts for new post

---
+++ _posts/2012-04-29-open-source-poll-results.markdown
@@ -0,0 +1,20 @@
+---
+layout: post
+title: Open Source Survey Results
+categories:
+ - open source
+---
+
+There's 
+
+<div id="chart"><!-- first chart goes here --></div>
+
+<script type="text/javascript" src="/public/raphael-min.js"> </script>
+<script type="text/javascript" src="/public/g.raphael-min.js"> </script>
+<script type="text/javascript" src="/public/g.bar-min.js"> </script>
+<script type="text/javascript" src="/public/backbone-min.js"> </script>
+<script type="text/javascript" src="/blog/open-source-charts.js"> </script>
+<script type="text/javascript" src="/blog/open-source-results.json"> </script>
+
+and more text just to block it off
+
---
+++ blog/open-source-charts.js
@@ -0,0 +1,52 @@
+function makeChart(name, numbers, labels, participants) {
+	console.log(participants);
+	var r = Raphael(name),
+		fin = function () {
+			r.barLabels[this.bar.id].animate({'stroke': '#AAAAAA'}, 300);
+		},
+		fout = function () {
+			r.barLabels[this.bar.id].animate({'stroke': '#000000'}, 300);
+		},
+		txtattr = { font: "12px sans-serif" };
+
+	var chart = r.barchart(10, 10, 300, 220, [numbers]).hover(fin, fout);
+
+	var yStart = 220;
+	r.barLabels = _.map(labels, function(text) {
+		var label = r.text(0, yStart, text).attr({'text-anchor': 'start', 'style': 'vertical-align: text-top'});
+		label.attr('y', yStart + (label.node.offsetHeight/2));
+		r.add(label);
+		yStart += label.node.offsetHeight;
+		return label;
+	});
+
+}
+
+function analyze(data) {
+	console.log(data);
+
+	function reasonsFor(reason, filter) {
+		var filtered = _.filter(data.results, filter);
+		var participants = _.size(filtered);
+		
+		var vals = _.reduce(filtered, function(mem, o) { 
+			for (key in o[reason]) {
+				mem[o[reason][key]] ++;
+			}
+			return mem;
+		}, _.map(data[reason], function(){ return 0; }));
+
+		var sortable = _.map(vals, function(val, i) { return { key: i, val: val }; });
+		var sorted = _.first(_.sortBy(sortable, function(pair) { return pair.val; }).reverse(), 5);
+		var vals = _.map(sorted, function(pair) { return pair.val; });
+		var labels = _.map(sorted, function(pair) { return data[reason][pair.key]; });
+		return { values: vals, labels: labels, participants: participants };
+	}
+
+	var chartData = reasonsFor('conReasons', function(o) { return o.frequency == 'Never' 
+		|| o.frequency == 'A few times a year' 
+		|| o.frequency == 'less than 5 times in my life'; 
+	});
+
+	makeChart("chart", chartData.values, chartData.labels, chartData.participants);
+}
